02b3853f1621a93cd01a0a11db66b62136cd853d
shree675 <shreeteshm37@gmail.com>
2021-05-02 19:03:20 +0530

    added full link

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connection.once('open', ()=>{
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('build'));
-    app.use('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname,'build/index.html'));
     });
 }
