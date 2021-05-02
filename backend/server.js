b6b23f404a95def20f2639e16e25bf92f23706ba
shree675 <shreeteshm37@gmail.com>
2021-05-02 17:11:42 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,9 @@ connection.once('open', ()=>{
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"build","index.html"));
+    });
 }
 
 const userRouter=require('./routes/user');
