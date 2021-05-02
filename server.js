b6cd89c37a1e7d2a2711bc63665b576aac447696
shree675 <shreeteshm37@gmail.com>
2021-05-02 19:27:28 +0530

    added correct config var

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port=process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-const uri=process.env.ATLAS_URI1;
+const uri=process.env.MONGODB_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 const connection = mongoose.connection;
 connection.once('open', ()=>{
