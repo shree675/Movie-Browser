e9863ccc0b00900c8baafeef265adff0d42ea889
shree675 <shreeteshm37@gmail.com>
2021-05-03 17:19:29 +0530

    redundant config

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app){
     app.use(proxy(['/api'],{target: 'http://localhost:5000'}));
+    app.use(proxy(['/'],{target: 'http://localhost:5000'}));
+    app.use(proxy(['/pref'],{target: 'http://localhost:5000'}));
 }
 
 app.listen(port, ()=>{
