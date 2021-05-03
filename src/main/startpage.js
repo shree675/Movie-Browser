4b4efc4d2c36c8cc307cc99b78790ba4f4fca9a4
shree675 <shreeteshm37@gmail.com>
2021-05-03 15:56:20 +0530

    added assets

--- src/main/startpage.js
+++ src/main/startpage.js
@@ -7,7 +7,7 @@ class StartPage extends Component {
 
     render(){
         return (
-            <div style={{width: `100%`}}>
+            <div style={{width: `100%`, height: `100%`, position: `relative`}}>
 
                 <ButtonAppBar />                        
 
@@ -17,7 +17,11 @@ class StartPage extends Component {
                     <div style={{fontSize: `30px`, fontWeight: `bold`, color: `#acacac`}}>Welcome to Movie Browser</div>
                     <div style={{fontSize: `18px`, color: `#acacac`, paddingTop: `28px`, paddingLeft: `100px`, paddingRight: `100px`}}>Get started by clicking on LOGIN</div>
                     <div style={{fontSize: `18px`, color: `#acacac`, padding: `5px`, paddingLeft: `100px`, paddingRight: `100px`}}>If this is your first time, then sign up for Movie Browser by clicking on SIGN UP</div>
-                    <div style={{fontSize: `18px`, color: `#acacac`, padding: `0px`, paddingLeft: `100px`, paddingRight: `100px`}}>You can change your prefernces in the Settings page upon login</div>
+                    <div style={{fontSize: `18px`, color: `#acacac`, padding: `0px`, paddingLeft: `100px`, paddingRight: `100px`}}>You can change your prefernces in the Settings page upon login</div>                    
+                </div>
+
+                <div className="tmdb-text">
+                This product uses the TMDb API but is not endorsed or certified by TMDb.
                 </div>
 
             </div>
